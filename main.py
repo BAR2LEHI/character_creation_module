03c102c43ac19886731a5981b5ed976c51572396
Владислав Тарасов <vat200@yandex.ru>
2023-01-04 17:45:55 +0300

    my commit

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     char_name = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Ты можешь выбрать один из трёх путей ff силы:')
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
